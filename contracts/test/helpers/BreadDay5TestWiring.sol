fd0aab9eac5a890b78197a945a7ca5f3dcda9adf
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-08 23:39:28 +0100

    test: centralize controlled graduation wiring

---
+++ contracts/test/helpers/BreadDay5TestWiring.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {BreadFeeEscrow} from "../../src/fees/BreadFeeEscrow.sol";
+import {BreadLaunchFactory} from "../../src/factory/BreadLaunchFactory.sol";
+import {BreadPermanentLiquidityLocker} from "../../src/graduation/BreadPermanentLiquidityLocker.sol";
+import {BreadEmergencyController} from "../../src/security/BreadEmergencyController.sol";
+import {IBreadLaunchFactory} from "../../src/interfaces/IBreadLaunchFactory.sol";
+import {IGraduationAdapter} from "../../src/interfaces/IGraduationAdapter.sol";
+import {MockGraduationAdapter} from "./MockGraduationAdapter.sol";
+import {MockGraduationCoordinator} from "./MockGraduationCoordinator.sol";
+import {MockUSDC6} from "./MockUSDC6.sol";
+
+library BreadDay5TestWiring {
+    bytes32 internal constant CONFIG_HASH = keccak256("BREAD_DAY5_CONTROLLED_TEST_GRADUATION");
+
+    function wire(
+        BreadLaunchFactory factory,
+        MockUSDC6 usdc,
+        BreadFeeEscrow escrow,
+        BreadEmergencyController emergencyController
+    ) internal returns (
+        BreadPermanentLiquidityLocker locker,
+        MockGraduationCoordinator coordinator,
+        MockGraduationAdapter adapter
+    ) {
+        locker = new BreadPermanentLiquidityLocker(address(this));
+        coordinator = new MockGraduationCoordinator(
+            address(factory), address(usdc), address(escrow), address(emergencyController), address(locker)
+        );
+        locker.setCoordinator(address(coordinator));
+        factory.setGraduationCoordinator(coordinator);
+
+        adapter = new MockGraduationAdapter(
+            IGraduationAdapter.AdapterFamily.UNISWAP_V4, address(usdc), address(locker), CONFIG_HASH
+        );
+        (IBreadLaunchFactory.LaunchConfig memory config,) = factory.currentLaunchConfig();
+        config.graduationAdapter = address(adapter);
+        config.graduationConfigHash = CONFIG_HASH;
+        config.enabled = true;
+        factory.setLaunchConfig(config);
+    }
+}
