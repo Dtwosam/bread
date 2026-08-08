f84378a67f14c4a4b73c840c94c8d7824cac621f
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-08 23:41:22 +0100

    test: generalize controlled USDC wiring

--- contracts/test/helpers/BreadDay5TestWiring.sol
+++ contracts/test/helpers/BreadDay5TestWiring.sol
@@ -8,12 +8,11 @@ import {IBreadLaunchFactory} from "../../src/interfaces/IBreadLaunchFactory.sol"
 import {IGraduationAdapter} from "../../src/interfaces/IGraduationAdapter.sol";
 import {MockGraduationAdapter} from "./MockGraduationAdapter.sol";
 import {MockGraduationCoordinator} from "./MockGraduationCoordinator.sol";
-import {MockUSDC6} from "./MockUSDC6.sol";
 
 library BreadDay5TestWiring {
     bytes32 internal constant CONFIG_HASH = keccak256("BREAD_DAY5_CONTROLLED_TEST_GRADUATION");
 
-    function wire(BreadLaunchFactory factory, MockUSDC6 usdc, BreadFeeEscrow escrow, address emergencyController)
+    function wire(BreadLaunchFactory factory, address usdc, BreadFeeEscrow escrow, address emergencyController)
         internal
         returns (
             BreadPermanentLiquidityLocker locker,
@@ -23,13 +22,13 @@ library BreadDay5TestWiring {
     {
         locker = new BreadPermanentLiquidityLocker(address(this));
         coordinator = new MockGraduationCoordinator(
-            address(factory), address(usdc), address(escrow), emergencyController, address(locker)
+            address(factory), usdc, address(escrow), emergencyController, address(locker)
         );
         locker.setCoordinator(address(coordinator));
         factory.setGraduationCoordinator(coordinator);
 
         adapter = new MockGraduationAdapter(
-            IGraduationAdapter.AdapterFamily.UNISWAP_V4, address(usdc), address(locker), CONFIG_HASH
+            IGraduationAdapter.AdapterFamily.UNISWAP_V4, usdc, address(locker), CONFIG_HASH
         );
         (IBreadLaunchFactory.LaunchConfig memory config,) = factory.currentLaunchConfig();
         config.graduationAdapter = address(adapter);
