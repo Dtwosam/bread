1530d9f692699b72a43860599afa3310710e9647
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-08 15:26:42 +0100

    test: reduce trading fixture stack usage

--- contracts/test/BreadBondingCurveTrading.t.sol
+++ contracts/test/BreadBondingCurveTrading.t.sol
@@ -16,16 +16,23 @@ contract BreadBondingCurveTradingTest {
     uint16 private constant TRADE_FEE_BPS = 100;
     uint16 private constant CREATOR_TAX_BPS = 500;
 
+    struct SellExpectations {
+        uint256 quoteIn;
+        uint256 buyFee;
+        uint256 buyTax;
+        uint256 buyNetQuote;
+        uint256 boughtTokens;
+        uint256 tokensIn;
+        uint256 quoteReserveBeforeSell;
+        uint256 tokenReserveBeforeSell;
+        uint256 grossQuoteOut;
+        uint256 sellFee;
+        uint256 sellTax;
+        uint256 expectedQuoteOut;
+    }
+
     function testOrdinaryBuyUsesQuoteLegFeesAndTrackedReserves() public {
-        (
-            MockUSDC6 usdc,
-            BreadFeePolicy policy,
-            BreadFeeEscrow escrow,
-            BreadBondingCurve curve,
-            BreadLaunchToken token
-        ) = _deployFixture();
-        policy;
-        escrow;
+        (MockUSDC6 usdc, BreadBondingCurve curve, BreadLaunchToken token) = _deployFixture();
 
         uint256 quoteIn = 1_000 * ONE_USDC;
         uint256 fee = quoteIn * TRADE_FEE_BPS / 10_000;
@@ -53,60 +60,44 @@ contract BreadBondingCurveTradingTest {
     }
 
     function testOrdinarySellChargesQuoteLegFeesAndReturnsNetQuote() public {
-        (
-            MockUSDC6 usdc,
-            BreadFeePolicy policy,
-            BreadFeeEscrow escrow,
-            BreadBondingCurve curve,
-            BreadLaunchToken token
-        ) = _deployFixture();
-        policy;
-        escrow;
+        (MockUSDC6 usdc, BreadBondingCurve curve, BreadLaunchToken token) = _deployFixture();
+        SellExpectations memory e;
 
-        uint256 quoteIn = 2_000 * ONE_USDC;
-        uint256 buyFee = quoteIn * TRADE_FEE_BPS / 10_000;
-        uint256 buyTax = quoteIn * CREATOR_TAX_BPS / 10_000;
-        uint256 buyNetQuote = quoteIn - buyFee - buyTax;
-        uint256 boughtTokens = _amountOut(buyNetQuote, PHANTOM_QUOTE, TOKEN_SUPPLY);
+        e.quoteIn = 2_000 * ONE_USDC;
+        e.buyFee = e.quoteIn * TRADE_FEE_BPS / 10_000;
+        e.buyTax = e.quoteIn * CREATOR_TAX_BPS / 10_000;
+        e.buyNetQuote = e.quoteIn - e.buyFee - e.buyTax;
+        e.boughtTokens = _amountOut(e.buyNetQuote, PHANTOM_QUOTE, TOKEN_SUPPLY);
 
-        usdc.mint(address(this), quoteIn);
-        assert(usdc.approve(address(curve), quoteIn));
-        curve.buy(quoteIn, boughtTokens, address(this));
+        usdc.mint(address(this), e.quoteIn);
+        assert(usdc.approve(address(curve), e.quoteIn));
+        curve.buy(e.quoteIn, e.boughtTokens, address(this));
 
-        uint256 tokensIn = boughtTokens / 4;
-        uint256 quoteReserveBeforeSell = PHANTOM_QUOTE + buyNetQuote;
-        uint256 tokenReserveBeforeSell = TOKEN_SUPPLY - boughtTokens;
-        uint256 grossQuoteOut = _amountOut(tokensIn, tokenReserveBeforeSell, quoteReserveBeforeSell);
-        uint256 sellFee = grossQuoteOut * TRADE_FEE_BPS / 10_000;
-        uint256 sellTax = grossQuoteOut * CREATOR_TAX_BPS / 10_000;
-        uint256 expectedQuoteOut = grossQuoteOut - sellFee - sellTax;
+        e.tokensIn = e.boughtTokens / 4;
+        e.quoteReserveBeforeSell = PHANTOM_QUOTE + e.buyNetQuote;
+        e.tokenReserveBeforeSell = TOKEN_SUPPLY - e.boughtTokens;
+        e.grossQuoteOut = _amountOut(e.tokensIn, e.tokenReserveBeforeSell, e.quoteReserveBeforeSell);
+        e.sellFee = e.grossQuoteOut * TRADE_FEE_BPS / 10_000;
+        e.sellTax = e.grossQuoteOut * CREATOR_TAX_BPS / 10_000;
+        e.expectedQuoteOut = e.grossQuoteOut - e.sellFee - e.sellTax;
 
-        assert(token.approve(address(curve), tokensIn));
-        uint256 quoteOut = curve.sell(tokensIn, expectedQuoteOut, address(this));
+        assert(token.approve(address(curve), e.tokensIn));
+        uint256 quoteOut = curve.sell(e.tokensIn, e.expectedQuoteOut, address(this));
 
-        assert(quoteOut == expectedQuoteOut);
-        assert(usdc.balanceOf(address(this)) == expectedQuoteOut);
-        assert(usdc.balanceOf(address(curve)) == quoteIn - expectedQuoteOut);
-        assert(token.balanceOf(address(this)) == boughtTokens - tokensIn);
-        assert(curve.quoteFeeBalance() == buyFee + sellFee);
-        assert(curve.creatorTaxBalance() == buyTax + sellTax);
-        assert(curve.trackedQuote() == quoteIn - expectedQuoteOut);
-        assert(curve.trackedTokens() == TOKEN_SUPPLY - boughtTokens + tokensIn);
+        assert(quoteOut == e.expectedQuoteOut);
+        assert(usdc.balanceOf(address(this)) == e.expectedQuoteOut);
+        assert(usdc.balanceOf(address(curve)) == e.quoteIn - e.expectedQuoteOut);
+        assert(token.balanceOf(address(this)) == e.boughtTokens - e.tokensIn);
+        assert(curve.quoteFeeBalance() == e.buyFee + e.sellFee);
+        assert(curve.creatorTaxBalance() == e.buyTax + e.sellTax);
+        assert(curve.trackedQuote() == e.quoteIn - e.expectedQuoteOut);
+        assert(curve.trackedTokens() == TOKEN_SUPPLY - e.boughtTokens + e.tokensIn);
         (uint256 quoteReserveAfter, uint256 tokenReserveAfter) = curve.getReserves();
-        assert(quoteReserveAfter == quoteReserveBeforeSell - grossQuoteOut);
-        assert(tokenReserveAfter == tokenReserveBeforeSell + tokensIn);
+        assert(quoteReserveAfter == e.quoteReserveBeforeSell - e.grossQuoteOut);
+        assert(tokenReserveAfter == e.tokenReserveBeforeSell + e.tokensIn);
     }
 
-    function _deployFixture()
-        private
-        returns (
-            MockUSDC6 usdc,
-            BreadFeePolicy policy,
-            BreadFeeEscrow escrow,
-            BreadBondingCurve curve,
-            BreadLaunchToken token
-        )
-    {
+    function _deployFixture() private returns (MockUSDC6 usdc, BreadBondingCurve curve, BreadLaunchToken token) {
         usdc = new MockUSDC6();
         BreadFeePolicySnapshot memory snapshot = BreadFeePolicySnapshot({
             protocolFeeRecipient: address(0xA11CE),
@@ -114,8 +105,8 @@ contract BreadBondingCurveTradingTest {
             protocolFeeShareBps: 2_500,
             maxCreatorTaxBps: 500
         });
-        policy = new BreadFeePolicy(address(this), snapshot, address(0xB0B));
-        escrow = new BreadFeeEscrow(address(usdc), address(this));
+        BreadFeePolicy policy = new BreadFeePolicy(address(this), snapshot, address(0xB0B));
+        BreadFeeEscrow escrow = new BreadFeeEscrow(address(usdc), address(this));
         curve = new BreadBondingCurve(
             address(usdc),
             address(this),
