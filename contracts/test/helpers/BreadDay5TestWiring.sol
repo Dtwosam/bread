ea6513e071b4d5bd47181bbb116b247d587e6940
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-08 23:40:14 +0100

    test: generalize emergency dependency in wiring helper

--- contracts/test/helpers/BreadDay5TestWiring.sol
+++ contracts/test/helpers/BreadDay5TestWiring.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.26;
 import {BreadFeeEscrow} from "../../src/fees/BreadFeeEscrow.sol";
 import {BreadLaunchFactory} from "../../src/factory/BreadLaunchFactory.sol";
 import {BreadPermanentLiquidityLocker} from "../../src/graduation/BreadPermanentLiquidityLocker.sol";
-import {BreadEmergencyController} from "../../src/security/BreadEmergencyController.sol";
 import {IBreadLaunchFactory} from "../../src/interfaces/IBreadLaunchFactory.sol";
 import {IGraduationAdapter} from "../../src/interfaces/IGraduationAdapter.sol";
 import {MockGraduationAdapter} from "./MockGraduationAdapter.sol";
@@ -14,19 +13,17 @@ import {MockUSDC6} from "./MockUSDC6.sol";
 library BreadDay5TestWiring {
     bytes32 internal constant CONFIG_HASH = keccak256("BREAD_DAY5_CONTROLLED_TEST_GRADUATION");
 
-    function wire(
-        BreadLaunchFactory factory,
-        MockUSDC6 usdc,
-        BreadFeeEscrow escrow,
-        BreadEmergencyController emergencyController
-    ) internal returns (
-        BreadPermanentLiquidityLocker locker,
-        MockGraduationCoordinator coordinator,
-        MockGraduationAdapter adapter
-    ) {
+    function wire(BreadLaunchFactory factory, MockUSDC6 usdc, BreadFeeEscrow escrow, address emergencyController)
+        internal
+        returns (
+            BreadPermanentLiquidityLocker locker,
+            MockGraduationCoordinator coordinator,
+            MockGraduationAdapter adapter
+        )
+    {
         locker = new BreadPermanentLiquidityLocker(address(this));
         coordinator = new MockGraduationCoordinator(
-            address(factory), address(usdc), address(escrow), address(emergencyController), address(locker)
+            address(factory), address(usdc), address(escrow), emergencyController, address(locker)
         );
         locker.setCoordinator(address(coordinator));
         factory.setGraduationCoordinator(coordinator);
