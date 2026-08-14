5176be1ea4104376ad1ff9940eda59c1b0f2e710
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-14 16:35:08 +0100

    style(day9): format V3 type contract RED

--- tests/day9/post-graduation-v3-types.test.ts
+++ tests/day9/post-graduation-v3-types.test.ts
@@ -2,13 +2,15 @@ import type {
   IndexedTokenTrade,
   TradeExecutionPriceSource,
   TradeVenueKind,
-} from '../../packages/types/src/index.js';
+} from "../../packages/types/src/index.js";
 
 type Equal<Left, Right> =
-  (<Value>() => Value extends Left ? 1 : 2) extends <Value>() =>
-    Value extends Right ? 1 : 2
-    ? (<Value>() => Value extends Right ? 1 : 2) extends <Value>() =>
-        Value extends Left ? 1 : 2
+  (<Value>() => Value extends Left ? 1 : 2) extends <
+    Value,
+  >() => Value extends Right ? 1 : 2
+    ? (<Value>() => Value extends Right ? 1 : 2) extends <
+        Value,
+      >() => Value extends Left ? 1 : 2
       ? true
       : false
     : false;
@@ -22,14 +24,19 @@ type ExpectedVenue = Readonly<{
 }>;
 
 type VenueKindsAreExact = Assert<
-  Equal<TradeVenueKind, 'BREAD_CURVE' | 'UNISWAP_V3'>
+  Equal<TradeVenueKind, "BREAD_CURVE" | "UNISWAP_V3">
 >;
 type PriceSourcesAreExact = Assert<
-  Equal<TradeExecutionPriceSource, 'CURVE_EXECUTION' | 'V3_SWAP_EXECUTION'>
+  Equal<TradeExecutionPriceSource, "CURVE_EXECUTION" | "V3_SWAP_EXECUTION">
 >;
-type PublicVenueIsExact = Assert<Equal<IndexedTokenTrade['venue'], ExpectedVenue>>;
+type PublicVenueIsExact = Assert<
+  Equal<IndexedTokenTrade["venue"], ExpectedVenue>
+>;
 type PublicPriceSourceUsesSharedContract = Assert<
-  Equal<IndexedTokenTrade['executionPrice']['source'], TradeExecutionPriceSource>
+  Equal<
+    IndexedTokenTrade["executionPrice"]["source"],
+    TradeExecutionPriceSource
+  >
 >;
 
 export type Day9V3TypeContractAssertions = readonly [
